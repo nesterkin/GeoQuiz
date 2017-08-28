08778cd644ac0d71fd99cee08573927d5f9ad72e
Nesterkin Alexander <nesterkin.a.s@sberbank.ru>
2017-08-28 14:57:40 +0300

    Обновление до главы 7

--- app/src/main/java/ru/twisterkin/geoquiz/CheatActivity.java
+++ app/src/main/java/ru/twisterkin/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package ru.twisterkin.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,6 +16,7 @@ public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE = "ru.twisterkin.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN = "ru.twisterkin.geoquiz.answer_shown";
+    private TextView mVersionTextView;
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
     private boolean mAnswerIsTrue;
@@ -25,6 +30,8 @@ public class CheatActivity extends AppCompatActivity {
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
+        mVersionTextView = (TextView) findViewById(R.id.version_text_view);
+
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,8 +41,35 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator animator = ViewAnimationUtils.createCircularReveal(mShowAnswerButton, cx,
+                            cy, radius, 0);
+                    animator.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
         });
+
+        mVersionTextView.setText(
+//                Build.VERSION.BASE_OS + " " +
+                Build.VERSION.CODENAME + " " +
+                Build.VERSION.INCREMENTAL + " " +
+//                Build.VERSION.PREVIEW_SDK_INT + " " +
+                Build.VERSION.RELEASE + " " +
+                Build.VERSION.SDK_INT
+//                + " " + Build.VERSION.SECURITY_PATCH
+        );
     }
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
